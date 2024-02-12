ee720e9995630c80522674e800f93faeb02d0a6e
AllRez44 <allanresendegomes@outlook.com>
2024-02-12 16:12:57 -0300

    add RSS lib & support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
+import { loadEnv } from "vite"
 import { defineConfig } from 'astro/config';
 
+const { APP_SITE } = loadEnv(process.env.APP_SITE, process.cwd(), "")
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: APP_SITE,
+});
---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,31 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import type { RSSFeedItem } from '@astrojs/rss';
+
+type Context = {
+    site: string | URL
+}
+
+export async function GET(context: Context) {
+    const title = 'Astro Learner | Blog';
+    const description = 'My journey learning Astro';
+
+    const items = await pagesGlobToRssItems(import.meta.glob('./**/*.md'));
+    const itemsFixed: RSSFeedItem[] = items.map((item) => {
+        const fixedItem = {
+            ...item,
+            link: item.link ? item.link : context.site.toString(),
+            title: item.title ? item.title : title,
+            pubDate: item.pubDate,
+            description: item.description ? item.description : description,
+        };
+        return fixedItem;
+    });
+
+    return rss({
+        title: title,
+        description: description,
+        site: context.site,
+        items: itemsFixed,
+        customData: `<language>en-us</language>`,
+    });
+}
