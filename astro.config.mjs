f789df8df45d67b4302fb7f8712a5674f3faac01
AllRez44 <allanresendegomes@outlook.com>
2024-02-13 13:19:27 -0300

    add ThemeIcon component & dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { loadEnv } from "vite"
+import { loadEnv } from "vite";
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+const {
+  APP_SITE
+} = loadEnv(process.env.APP_SITE, process.cwd(), "");
 
-const { APP_SITE } = loadEnv(process.env.APP_SITE, process.cwd(), "")
 
 // https://astro.build/config
 export default defineConfig({
-    site: APP_SITE,
+  site: APP_SITE,
+  integrations: [preact()]
 });
